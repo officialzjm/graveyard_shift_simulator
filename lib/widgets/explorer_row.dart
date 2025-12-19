a0d6289d6eb905884fc1e2cf9a55613ba45be684
brayden-hayes <brhayes02@gmail.com>
2025-12-19 21:37:20 +0000

    update

--- lib/widgets/explorer_row.dart
+++ lib/widgets/explorer_row.dart
@@ -125,7 +125,7 @@ class CommandRow extends StatelessWidget {
                   isExpanded: true,
                   onChanged: (CommandName? newName) {
                     if (newName != null) {
-                      commandList.modifyCommand(index, Command(t: command.t, name: newName));
+                      commandList.modifyCommand(index, Command(t: command.t, waypointIndex: i, name: newName));
                     }
                   },
                   items: CommandName.values.map((cmd) {
