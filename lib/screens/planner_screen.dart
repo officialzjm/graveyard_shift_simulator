43907d96e955d4a6f0dfd0854b3f2462093148d2
officialzjm <officialzjm2024@gmail.com>
2025-12-20 12:16:57 -0500

    Update planner_screen.dart

--- lib/screens/planner_screen.dart
+++ lib/screens/planner_screen.dart
@@ -67,7 +67,7 @@ class _PlannerScreenState extends State<PlannerScreen> {
                           final result = importPathJson(jsonString);
 
                           context.read<PathModel>().setPath(result); //not optimal to place it here so i will move it later
-                          contest.read<CommandList>().setCommands(result.commands);
+                          context.read<CommandList>().setCommands(result.commands);
                           //commandList.setCommands(result.commands);
                         } catch (err) {
                           ScaffoldMessenger.of(context).showSnackBar(
