a8c6e79c6dae40730aa995bd1fa141effbbb59ca
officialzjm <officialzjm2024@gmail.com>
2025-12-20 10:17:35 -0500

    Converted exported accel to meters

--- lib/jsonconversion.dart
+++ lib/jsonconversion.dart
@@ -59,7 +59,7 @@ List<Segment> createSegmentList(List<Waypoint> waypoints) {
     } else {
       path = [wp.pos,nextWp.pos];
     }
-    segments.add(Segment(inverted: wp.reversed, stopEnd: false, path: path, velocity: (wp.velocity*0.0254).toPrecision(4), accel: wp.accel.toPrecision(4)));
+    segments.add(Segment(inverted: wp.reversed, stopEnd: false, path: path, velocity: (wp.velocity*0.0254).toPrecision(4), accel: (wp.accel*0.0254).toPrecision(4)));
   }
   return segments;
 }
