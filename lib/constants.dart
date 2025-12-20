4f1446bd419b793d661717bf1b5e4bfb354304fb
officialzjm <officialzjm2024@gmail.com>
2025-12-20 10:11:51 -0500

    Added accel constraint

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,6 @@
+//Speed and Velocity Variables
+//Inches per second , we will standarize to meters later
 const double maxVelocity = 70;
+const double maxAccel = 200; // im not sure what this should be
 const double fieldHalf = 72.6;
 const handleRadius = 2.0;
